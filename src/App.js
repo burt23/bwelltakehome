4eaec0b5a2ee7b9d811019a3aeab56e30fa8e781
Michael Burton <mail4mdb@gmail.com>
2021-03-10 11:41:29 -0600

    stub out useEffect to call for mock json data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const url = "http://hapi.fhir.org/baseR4/Patient?_format=json";
+
+const fetchData = async () => {
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+};
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    async function getData() {
+      const d = await fetchData();
+      setData(d);
+    }
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
