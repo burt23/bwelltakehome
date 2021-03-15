655b6d88462f2bc18df499df5b165f88ce3bc9c5
Michael Burton <mail4mdb@gmail.com>
2021-03-15 10:36:21 -0500

    fix typo in birthDate of object destructuring

--- src/Components/PatientRecord.js
+++ src/Components/PatientRecord.js
@@ -2,7 +2,7 @@ import { TableRow, TableCell } from "@material-ui/core";
 
 const PatientRecord = ({ record }) => {
   const {
-    resource: { name = [], birthdate = "dob", gender = "gender" },
+    resource: { name = [], birthDate = "n/a", gender = "n/a" },
     fullUrl,
   } = record;
   const { family = "", given = "" } = name[0] || {};
@@ -14,7 +14,7 @@ const PatientRecord = ({ record }) => {
         {builtName}
       </TableCell>
       <TableCell align="right">{gender}</TableCell>
-      <TableCell align="right">{birthdate}</TableCell>
+      <TableCell align="right">{birthDate}</TableCell>
       <TableCell align="right">
         <a alt="showMoreLink" href={fullUrl}>
           Show More
